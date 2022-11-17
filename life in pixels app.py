f3510bc39e80ba4c635fd0627a5bf9f42a20b004
Jakub-Venglar <jakub.venglar@seznam.cz>
2022-11-17 01:06:40 +0100

    design basics

--- life in pixels app.py
+++ life in pixels app.py
@@ -1,4 +1,16 @@
 #! python3
-#import kivy
-import kivy
-print ('test')
+from kivy.app import App
+from kivy.uix.widget import Widget
+
+class LifeLayout(Widget):
+    pass
+
+
+class LifePixels(App):
+    def build(self):
+        return LifeLayout()
+
+
+if __name__ == "__main__":
+
+    LifePixels().run()
