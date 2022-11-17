6920969fed6fc015803f13b3e78d3727e012a673
Jakub-Venglar <jakub.venglar@seznam.cz>
2022-11-17 13:13:08 +0100

    basic calendar view

--- life in pixels app.py
+++ life in pixels app.py
@@ -1,15 +1,31 @@
 #! python3
-from kivy.app import App
-from kivy.uix.widget import Widget
+import calendar
+from datetime import datetime
+from kivymd.app import MDApp
+from kivymd.uix.widget import MDWidget
 
-class LifeLayout(Widget):
-    pass
+class LifeLayout(MDWidget):
+    def makeCal(self):
+        c = calendar.Calendar(0)
+        calList = [['1-1','1-2','1-3','1-4','1-5','1-6','1-7'],
+                    ['2-1','2-2','2-3','2-4','2-5','2-6','2-7'],
+                    ['3-1','3-2','3-3','3-4','3-5','3-6','3-7'],
+                    ['4-1','4-2','4-3','4-4','4-5','4-6','4-7'],
+                    ['5-1','5-2','5-3','5-4','5-5','5-6','5-7']]
+        currentCal = c.monthdatescalendar(datetime.now().year, datetime.now().month)
+        for weeknum, week in enumerate(calList):
+            for daynum, day in enumerate(week):
+                id = calList[weeknum][daynum]
+                self.ids[id].text = str(currentCal[weeknum][daynum].day)
 
 
-class LifePixels(App):
+        #print(currentCal[0][0].day)
+
+class LifePixels(MDApp):
     def build(self):
         return LifeLayout()
-
+    def on_start(self):
+        self.root.makeCal()
 
 if __name__ == "__main__":
 
